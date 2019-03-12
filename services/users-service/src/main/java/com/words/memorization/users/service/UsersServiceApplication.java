511c0874b9e215c093115ae4a3271f66278b7696
Dmitriy Konoplev <konoplev@yarnord.ru>
2019-03-12 15:39:40 +0300

    Changed configuration

--- services/users-service/src/main/java/com/words/memorization/users/service/UsersServiceApplication.java
+++ services/users-service/src/main/java/com/words/memorization/users/service/UsersServiceApplication.java
@@ -3,9 +3,11 @@ package com.words.memorization.users.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @EnableEurekaClient
+@PropertySource(value = "classpath:hibernate-jpa.properties")
 public class UsersServiceApplication {
 
     public static void main(String[] args) {
