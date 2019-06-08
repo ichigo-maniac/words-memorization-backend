2757200722d9e3272b1d7b2cb5d7bf75cbd3f4c0
impi <ichigomaniac@gmail.com>
2019-06-08 07:34:09 +0300

    Added validation annotations

--- facades/words-management-facade/src/main/java/com/words/memorization/words/management/facade/services/KanjiClientService.java
+++ facades/words-management-facade/src/main/java/com/words/memorization/words/management/facade/services/KanjiClientService.java
@@ -3,14 +3,15 @@ package com.words.memorization.words.management.facade.services;
 import com.words.memorization.words.facades.common.dto.KanjiDto;
 import com.words.memorization.words.management.facade.api.model.PostKanjiInput;
 
+import javax.validation.constraints.NotNull;
 import java.util.UUID;
 
 public interface KanjiClientService {
 
-    KanjiDto getKanjiByDisplayText(String displayText);
+    KanjiDto getKanjiByDisplayText(@NotNull String displayText);
 
-    KanjiDto getKanjiById(UUID kanjiId);
+    KanjiDto getKanjiById(@NotNull UUID kanjiId);
 
-    KanjiDto createKanji(PostKanjiInput kanjiInput);
+    KanjiDto createKanji(@NotNull PostKanjiInput kanjiInput);
 
 }
--- facades/words-management-facade/src/main/java/com/words/memorization/words/management/facade/services/WordsClientService.java
+++ facades/words-management-facade/src/main/java/com/words/memorization/words/management/facade/services/WordsClientService.java
@@ -3,14 +3,15 @@ package com.words.memorization.words.management.facade.services;
 import com.words.memorization.words.facades.common.dto.WordDto;
 import com.words.memorization.words.management.facade.api.model.PostWordInput;
 
+import javax.validation.constraints.NotNull;
 import java.util.UUID;
 
 public interface WordsClientService {
 
-    WordDto getWordByDisplayText(String displayText);
+    WordDto getWordByDisplayText(@NotNull String displayText);
 
-    WordDto getWordById(UUID wordId);
+    WordDto getWordById(@NotNull UUID wordId);
 
-    WordDto createWord(PostWordInput wordInput);
+    WordDto createWord(@NotNull PostWordInput wordInput);
 
 }
